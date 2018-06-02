0b6b196ece591fa94154397005d963600dcb3c62
Frixu <frixu@frixu.me>
2018-06-02 12:35:59 +0200

    New repo

---
+++ Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Frixu.BingoGen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! There will be something really soon.");
+        }
+    }
+}
